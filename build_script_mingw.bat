3f80b02676d07671fa4188d5802ceca2f0de503b
Han Sang-jin <tinysun.net@gmail.com>
2018-04-21 12:46:19 +0900

    Update build_script_mingw.bat

--- build_script_mingw.bat
+++ build_script_mingw.bat
@@ -5,6 +5,7 @@ rem Build clang
  cd %WORK_DIR%/build/NinjaMinGW/llvm
  cmake -G Ninja -D CMAKE_BUILD_TYPE=RELEASE -DCMAKE_C_COMPILER=clang  -DCMAKE_CXX_COMPILER=clang++ -DLLVM_ENABLE_ASSERTIONS:BOOL=TRUE %WORK_DIR%/llvm
 rem Use artifacts previously created by ninja
- wget -q https://ci.appveyor.com/api/projects/tinysun212/llvm-prebuilder/artifacts/swift_llvm_cache.tar.gz
+ SET JOB_NAME=Environment: PLATFORM=mingw64
+ wget -q https://ci.appveyor.com/api/projects/tinysun212/llvm-prebuilder/artifacts/swift_llvm_cache.tar.gz?job="%JOB_NAME%"
  tar zxf swift_llvm_cache.tar.gz
  ninja
