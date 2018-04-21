69ee6907a1ad1f2be2a34303c5e913567ff4ee9d
Han Sang-jin <tinysun.net@gmail.com>
2018-04-21 12:48:31 +0900

    Update build_script_msvc.bat

--- build_script_msvc.bat
+++ build_script_msvc.bat
@@ -6,7 +6,8 @@ rem Build clang
  CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
  cmake -G "Ninja" -DCMAKE_C_COMPILER=clang-cl -DCMAKE_CXX_COMPILER=clang-cl -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_CXX_FLAGS="-DLLVM_ENABLE_DUMP" ..\..\..\llvm
 rem Use artifacts previously created by ninja
- wget -q https://ci.appveyor.com/api/projects/tinysun212/llvm-prebuilder-163tm/artifacts/swift_llvm_cache.tar.gz
+ SET JOB_NAME=Environment: PLATFORM=msvc
+ wget -q https://ci.appveyor.com/api/projects/tinysun212/llvm-prebuilder/artifacts/swift_llvm_cache.tar.gz?job="%JOB_NAME%"
  7z x swift_llvm_cache.tar.gz -bd -bso0
  7z x swift_llvm_cache.tar -aoa -bd -bso0
  ninja
